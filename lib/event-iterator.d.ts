1e424dc11ee494dd5669d9f0b7b6343d9518a0bb
Alan Shaw <alan.shaw@protocol.ai>
2019-03-28 10:02:19 +0000

    feat: optional high water warning message
    This PR makes the high water warning message optional and disabled by default. It also allows the high water mark to be configured so users can customise when the warning is displayed.
    
    Also mocha config is updated so tests can run again!
    
    resolves #1

--- lib/event-iterator.d.ts
+++ lib/event-iterator.d.ts
@@ -3,10 +3,15 @@ export declare type StopCallback<T> = () => void;
 export declare type FailCallback<T> = (err: Error) => void;
 export declare type ListenHandler<T> = (push: PushCallback<T>, stop: StopCallback<T>, fail: FailCallback<T>) => void;
 export declare type RemoveHandler<T> = (push: PushCallback<T>, stop: StopCallback<T>, fail: FailCallback<T>) => void;
+declare type EventIteratorOptions = {
+    highWaterMark?: Number;
+    highWaterWarning?: Boolean;
+};
 export declare class EventIterator<T> implements AsyncIterable<T> {
     private listen;
     private remove?;
-    constructor(listen: ListenHandler<T>, remove?: RemoveHandler<T>);
+    private options;
+    constructor(listen: ListenHandler<T>, remove?: RemoveHandler<T>, options?: EventIteratorOptions);
     [Symbol.asyncIterator](): AsyncIterator<T>;
 }
 export default EventIterator;
